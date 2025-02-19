7cfdffe00bfcad9c9b5edf43d40659c9bfa00662
Deepak910-N <deepakrohit910@gmail.com>
2025-02-19 11:30:54 +0530

    first commit

---
+++ pitch_ai/src/main/java/com/deepak/beggg/pitch_ai/PitchAiApplication.java
@@ -0,0 +1,13 @@
+package com.deepak.beggg.pitch_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PitchAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PitchAiApplication.class, args);
+	}
+
+}
---
+++ pitch_ai/src/test/java/com/deepak/beggg/pitch_ai/PitchAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepak.beggg.pitch_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PitchAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
